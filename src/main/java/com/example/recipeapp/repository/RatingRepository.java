10241e2613a09b099bce3875641bb7ada87d622c
Luca Sicoli <lucasicoli@Mac.fibertel.com.ar>
2025-07-16 22:35:02 -0300

    final backend commit by luk

--- src/main/java/com/example/recipeapp/repository/RatingRepository.java
+++ src/main/java/com/example/recipeapp/repository/RatingRepository.java
@@ -19,8 +19,8 @@ public interface RatingRepository extends JpaRepository<Rating, Long> {
     // Método para verificar si ya existe un rating para un usuario y una receta
     Optional<Rating> findByUserIdAndRecipeId(Long userId, Long recipeId);
 
-    // Calcular el promedio de ratings para una receta
-    @Query("SELECT AVG(r.puntos) FROM Rating r WHERE r.recipe.id = :recipeId")
+    // Calcular el promedio de ratings SOLO de los APROBADOS para una receta
+    @Query("SELECT AVG(r.puntos) FROM Rating r WHERE r.recipe.id = :recipeId AND r.estadoAprobacion = 'APROBADO'")
     Double findAverageRatingByRecipeId(@Param("recipeId") Long recipeId);
 
     long countByUserId(Long userId);
